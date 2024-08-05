ff851860e20acc4a49618ea3023312e6196e97aa
William Roberts <wr7@sanger.ac.uk>
2024-08-05 09:42:50 +0100

    Update plot_annotation_script.py to use other looping strategy

--- bin/plot_annotation_coverage.py
+++ bin/plot_annotation_coverage.py
@@ -20,8 +20,7 @@ logging.basicConfig(
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--sample_1', '-s1', dest='sample_1', help="Name of a sample for which to show coverage")
-    parser.add_argument('--sample_2', '-s2', dest='sample_2', help="Name of a second sample for which to show coverage", required=False)
+    parser.add_argument('--sample', '-s', dest='sample', help="Name of a sample for which to show coverage")
     parser.add_argument('--gff', '-g', dest="gff", help="Genome annotation in GFF3 format", type=Path)
     parser.add_argument('--wig_dir', '-d', dest="wig_dir", help="Path to directory containing .wig coverage files", type=Path)
     parser.add_argument('--ext', '-e', dest="ext", help="Number of bases by which to extend coverage plot either side of an annotated gene", type=int, default=100)
@@ -49,10 +48,10 @@ def get_gene_name(ann_row: pd.DataFrame):
         return lt, lt
 
 
-def load_data(name_id: str, data_dir: Path, normalize: bool = False) -> tuple[pd.DataFrame, pd.DataFrame]:
+def load_data(sample_id: str, data_dir: Path, normalize: bool = False) -> tuple[pd.DataFrame, pd.DataFrame]:
     """Load wig files, extract the coverage column, normalise the coverage, collect into dfs for plus and minus strand"""
     plus_data, minus_data = [], []
-    for i, f in enumerate(data_dir.glob(f"{name_id}*plus.wig"), start=1):
+    for i, f in enumerate(data_dir.glob(f"{sample_id}_REP*plus.wig"), start=1):
         # Extract coverage (and label replicate R1, R2, etc.)
         p_data = pd.read_csv(f, sep="\t", header=None, names=['chrom', 'pos', 'cov'])['cov']
         #TODO fix potential bug if replacing plus with minus in full path (rather than just filename)!
@@ -69,21 +68,6 @@ def load_data(name_id: str, data_dir: Path, normalize: bool = False) -> tuple[pd
             plus_data.append(p_data)
             minus_data.append(m_data)
     return pd.concat(plus_data, axis=1), pd.concat(minus_data, axis=1)
-
-def add_coverage_data_context(coverage_data: dict[str, pd.DataFrame], ext: int):
-    extended_coverage_data = {}
-    for k, v in coverage_data.items():
-        chr_len = len(v)
-        v = v.reindex(range(1, chr_len + 1))
-        print(v)
-        lower_context = v[(chr_len - ext):(chr_len + 1)]
-        lower_context = lower_context.reindex(range(-1 - ext, 0)) 
-        upper_context = v[0:ext]
-        upper_context.reindex(range(chr_len + 1, chr_len + ext + 1))
-        extended_coverage_data[k] = pd.concat([lower_context, v, upper_context])
-        print(extended_coverage_data[k][0:102])
-        # print(extended_coverage_data[k][-101])
-        # print(extended_coverage_data[k][1139616:1139717])
 
 
 def get_gene_annotations(gff: Path) -> pd.DataFrame:
@@ -127,19 +111,7 @@ def add_intergenic_region_annotations(ann_base: pd.DataFrame, min_length: int = 
         '0'
     ])
     return ann
-
-class feature:
-    """Feature in genome, could be gene or intergenic region"""
-    def __init__(self, id, name, start, end, strand, tags, prev, next):
-        self.id
-        self.name
-        self.start
-        self.end
-        self.strand
-        self.tags
-        self.prev_gene
-        self.next_gene
-
+    
 
 def generate_gene_with_neighbours(ann: list) -> Generator:
     for i in range(len(ann)):
@@ -165,6 +137,20 @@ def generate_gene_with_neighbours(ann: list) -> Generator:
             j = 0
         yield ann[h], ann[i], ann[j]  # prev, gene, next
 
+
+def get_coverage_context(coverage_data: pd.DataFrame, gene, chr_len):
+    if gene[3] < gene[2]:
+        gene[3] += chr_len
+    start, end = get_region_limits(gene)
+    if start < 0:
+        new_cov_data = np.concatenate((coverage_data[start:], coverage_data[:end]))
+    elif end > chr_len:
+        new_cov_data = np.concatenate((coverage_data[start:], coverage_data[:end-chr_len]))
+    else:
+        new_cov_data = coverage_data[start:end]
+    return new_cov_data
+
+
 def get_region_limits(gene: list, ext: int = 100) -> tuple[int, int]:
     """
     Get the plotting limits for a regions corresponding to the given gene
@@ -177,108 +163,77 @@ def get_region_limits(gene: list, ext: int = 100) -> tuple[int, int]:
     Returns:
         tuple[int, int]: Limits of the region (for plotting)
     """
+    #TODO Original code had start = (gene[2] - ext - 1). Was that better?
     return (gene[2] - ext), (gene[3] + ext)
 
-def get_plot_data(coverage_data: dict[str, pd.DataFrame], region_limits: tuple) -> dict[str, dict]:
-    """
-    Generate data required for plotting coverage across loci in genome
-
-    Args:
-        coverage_data (dict): Dictionary mapping coverage data by strand_sample
-        region_limits (tuple): Start and end of the region to be plotted
-    """
-    start, end = region_limits
-    if len(coverage_data) == 2:
-        plot_data = {
-            'sense_1': {'color': 'b'}, 'anti_1': {'color': 'r'},
-        }
-    elif len(coverage_data) == 4:
-        plot_data = {
-            'sense_1': {'color': 'b'}, 'anti_1': {'color': 'r'},
-            'sense_2': {'color': 'c'}, 'anti_2': {'color': 'm'},
-        }
-    else:
-        raise ValueError(f"Unexpected number of elements in coverage data: ${coverage_data}")
-
-    for k, v in coverage_data.items():
-        # Find mean, min, max coverage across all replicates for this gene's region (on both strands, and in both samples)
-        plot_data[k]['values'] = v.iloc[start:end].mean(1).to_numpy()
-        plot_data[k]['min'] = v.iloc[start:end].min(1).to_numpy()
-        plot_data[k]['max'] = v.iloc[start:end].max(1).to_numpy()
-    return plot_data
 
 def get_chr_len(raw_coverage: pd.DataFrame) -> int:
     return len(raw_coverage)
+
 
 def get_fragment_size(gene: list) -> int:
     """Size of a gene or feature"""
     return gene[3] - gene[2] + 1  # + 1 because GFF coordinates are inclusive
 
-def get_region_size(region_limits: tuple[int, int]) -> int:
-    """Size of the region that is shown in a plot"""
-    return region_limits[1] - region_limits[0]
 
-def plot_base_line_graph(plot_data: dict, region_limits: tuple) -> tuple[matplotlib.figure.Figure, matplotlib.axes.Axes]:
-    fig, ax1 = plt.subplots()
-    ax1.set_xlim(*region_limits)
+def plot_coverage(axis: matplotlib.axes.Axes, coverage_data: pd.DataFrame, gene: list, line_style: str = '-') -> matplotlib.axes.Axes:
+    start, end = get_region_limits(gene)
+    axis.set_xlim(start, end)
+    axis.plot(range(start, end), coverage_data, line_style)
+    return axis
 
-    # Line graph of coverage
-    for label, info in plot_data.items():
-        x_range = range(*region_limits)
-        ax1.plot(x_range, info['values'], info['color'] + '-')  # mean
-        ax1.fill_between(x_range, info['min'], info['max'], color=info['color'], alpha=0.1)  # uncertainty
-    return fig, ax1
+
+def mark_chr_len(gene: list, chr_len: int):
+    start, end = get_region_limits(gene)
+    if end > chr_len:
+        plt.axvline(x=chr_len, color='k', lw=2, ls="-")
+
 
 def mark_gene_boundaries(gene: list) -> None:
     """
     Mutates currently active plot with vertical lines denoting annotated start and end of gene
     """
-    plt.axvline(x=gene[2], color='grey', lw=1, ls="--")
-    plt.axvline(x=gene[3], color='grey', lw=1, ls="--")
+    gene_start, gene_end = gene[2], gene[3]
+    plt.axvline(x=gene_start, color='grey', lw=1, ls="--")
+    plt.axvline(x=gene_end, color='grey', lw=1, ls="--")
+    
 
-def highlight_neighbouring_genes(ax1: matplotlib.axes.Axes, gene_and_neighbours: tuple, chr_len: int):
-    prev, gene, next = gene_and_neighbours
-    # Assign overlap regions and their color
-    # Choose colour for left and right ranges (100bp) either side of gene
-    left_col = 'r' if prev[4] == '-' else 'b'
-    right_col = 'r' if next[4] == '-' else 'b'
-    max_val = ax1.get_ylim()[1]
-    start, end = ax1.get_xlim()
-    # left_box = coordinate of end of previous feature
-    left_box = prev[3]
-    # Draw horizontal lines at top of plot indicating whether next/prev feature is on + or - strand
-    # Given that we extend the genes by 100bp, this handles the possibility of the extended region overlapping with the (unextended) range of the previous or next gene.
-    if left_box > start:
-        # if the the end of the prev gene is within the start of the (extended) region/plot
-        # the highlight the from start of the plot to the end of the prev gene
-        ax1.hlines(y=max_val, xmin=start, xmax=left_box+1, color=left_col, lw=10, alpha=0.5)
-    if end > chr_len and next[2] < (end - chr_len):  # last gene
-        # if end of the (extended) region/plot is beyond the end of the ref
-        # and the start of the next gene (probably the first) is before the end of the extended region/plot (last gene),
-        # then highlight between the start of the next gene and the end of the region/plot
-        ax1.hlines(y=max_val, xmin=next[2]+chr_len, xmax=end, color=right_col, lw=10, alpha=0.5)  # next[2] should be 0?
-        plt.axvline(x=chr_len, color='k', lw=2, ls="-")  # Mark the end of the ref with a solid line
-    if next[2] < end and next[3] > gene[2]:
-        # if the start of the next gene is within the end of the (extended) region/plot
-        # and the end of the next gene is beyond the start of the current gene
-        # then highlight between the start of the next gene and the end of the plot
-        ax1.hlines(y=max_val, xmin=next[2], xmax=end, color=right_col, lw=10, alpha=0.5)
+def highlight_neighbouring_genes(axis: matplotlib.axes.Axes, gene_with_neighbours: tuple[list, list, list], chr_len: int,  colours: tuple[str, str] = ('b','r')):
+    prev, gene, next = gene_with_neighbours
+    start, end = get_region_limits(gene)
+    max_val = axis.get_ylim()[1]
+    sense_col, anti_col = colours
+    left_col = anti_col if prev[4] == '-' else sense_col
+    right_col = anti_col if next[4] == '-' else sense_col
+    prev_end = prev[3] - chr_len if start < 0 else prev[3]
+    next_start = next[2] + chr_len if next[2] < gene[3] else next[2]
+    if prev_end > start:
+        axis.hlines(y=max_val, xmin=start, xmax=prev_end + 1, color=left_col, lw=10, alpha=0.5)
+    if next_start < end:
+        axis.hlines(y=max_val, xmin=next_start, xmax=end, color=right_col, lw=10, alpha=0.5)
+    return axis
 
-def add_gene_arrows(ax1: matplotlib.axes.Axes, gene: list, region_size: tuple[int], fragment_size: int):
+
+def add_gene_arrows(ax1: matplotlib.axes.Axes, gene: list, colours: tuple[str, str] = ('b', 'r')):
     # Because the max value changes if we add something near the top of the plot, the ylim has changed from what it was previously.
     # This causes next/prev gene annotations (if present) to appear below the (current) gene's arrow, but the plot still looks good!
+    sense_col, anti_col = colours
     max_val = ax1.get_ylim()[1]
+    start, end = get_region_limits(gene)
+    region_size = end - start
+    fragment_size = get_fragment_size(gene)
     arrow_kwargs = {
             "width": max_val / 50,  # simply 50x less than the height of the plot
             "length_includes_head": True,
             "head_length": region_size / 25,
         }
     if gene[4] == '+':
-        ax1.arrow(gene[2], max_val, fragment_size, 0,  facecolor='blue', **arrow_kwargs)
+        ax1.arrow(gene[2], max_val, fragment_size, 0,  facecolor=sense_col, **arrow_kwargs)
     elif gene[4] == '-':
-        ax1.arrow(gene[3], max_val, -fragment_size, 0, facecolor='red', **arrow_kwargs)
+        ax1.arrow(gene[3], max_val, -fragment_size, 0, facecolor=anti_col, **arrow_kwargs)
 
-def add_plot_title(gene, fragment_size):
+def add_plot_title(gene):
+    fragment_size = get_fragment_size(gene)
     strand = {"+": "+ strand", "-": "- strand", "0": ""}
         # If there's no readable gene name
     if gene[1] == gene[0]:
@@ -287,105 +242,66 @@ def add_plot_title(gene, fragment_size):
         # If there's a readable gene name
         plt.title(f"{gene[1]} ({gene[0]}) [{fragment_size} bp] {strand[gene[4]]}")
 
-def update_x_axis(ax1: matplotlib.axes.Axes, chr_len: int, region_size: tuple[int, int]):
-    start, end = ax1.get_xlim()
+def update_x_axis(axis: matplotlib.axes.Axes, chr_len: int):
+    start, end = axis.get_xlim()
+    region_size = end - start
+    print(region_size)
     if region_size < 400:
         step_size = 100 * (region_size // 200)
     else:
         step_size = 100 * (region_size // 400)
-        # start from the start of the region rounded up to the nearest hundred, cover the whole region, in step sizes described above
-    ax1.xaxis.set_ticks(np.arange(100 * (start // 100 + 1), end, step_size))
-    ax1.xaxis.set_major_formatter(FuncFormatter(lambda x, p: format(int(x % chr_len), ',')))
+    # start from the start of the region rounded up to the nearest hundred, cover the whole region, in step sizes described above
+    ticks = np.arange(100 * (start // 100 + 1), end, step_size)
+    axis.xaxis.set_ticks(ticks)
+    if start >= 10**6:
+        # Update font size (or could rotate) to avoid label clash
+        axis.set_xticklabels(ticks, fontsize=8)
+        # axis.set_xticklabels(ticks, rotation=45, ha='right')
+    axis.xaxis.set_major_formatter(FuncFormatter(lambda x, p: format(int(x % chr_len), ',')))
+
 
 def write_plot_to_file(save_path: Path, gene: list, i: int, num_ann: int):
     zfill_size = int(np.ceil(np.log10(num_ann)))
     readable_gene_name = gene[1]
     plt.savefig(f"{save_path}/feature_{str(i).zfill(zfill_size)}_{readable_gene_name}.png")
 
-def contextualize_coordinates(start: int, end: int, chr_len: int):
-    # Account for region where end overlaps with chromosome end
-    # TODO This will never happen if we are simply extending the chromosome with context...
-    if end < start:
-        end += chr_len + 1  #TODO + 1 because 1-based indexing
-    # Account for region where start overlaps with chromosome end
-    if start < 0:
-        start += chr_len + 1  #TODO + 1 because 1-based indexing
-    return start, end
-
-def contextualize_feature(feature: list, chr_len: int):
-    feature = feature.copy()
-    feature[2], feature[3] = contextualize_coordinates(feature[2], feature[3], chr_len)
-    return feature
-
-def contextualize_features(features: list, chr_len: int):
-    new_features = []
-    for feature in features:
-        new_features.append(contextualize_feature(feature, chr_len))
-    return new_features
-    
 
 def main():
     args = parse_args()
 
-    # Parse wig files for given sample into dfs of normalised per base coverage count data
-    if args.sample_2 is None:
-        save_path = args.outdir / f"{args.sample_1}"  #TODO Keep sample pair named subdirectory if we now give the user the choice of where to save? Probably not...
-        p1, m1 = load_data(args.sample_1, args.wig_dir)
-        coverage_data = {
-            'sense_1': p1,
-            'anti_1': m1,
-        }
-    else:
-        save_path = args.outdir / f"{args.sample_1}_{args.sample_2}"  #TODO Keep sample pair named subdirectory if we now give the user the choice of where to save? Probably not...
-        p1, m1 = load_data(args.sample_1, args.wig_dir)
-        p2, m2 = load_data(args.sample_2, args.wig_dir)
-        coverage_data = {
-            'sense_1': p1,
-            'anti_1': m1,
-            'sense_2': p2,
-            'anti_2': m2
-        }
+    save_path = args.outdir / f"{args.sample}"  #TODO Keep sample pair named subdirectory if we now give the user the choice of where to save? Probably not...
     os.makedirs(save_path, exist_ok=True)
-    chr_len = get_chr_len(p1)
 
-    add_coverage_data_context(coverage_data, 100)
+    # Parse wig files for given sample into dfs per base coverage data
+    p1, m1 = load_data(args.sample, args.wig_dir)
+    chr_len = get_chr_len(p1)
 
     # Parse GFF
     ann_base = get_gene_annotations(args.gff)
     ann = add_intergenic_region_annotations(ann_base, args.min_intergenic)
-    # print(ann[-2], ann[-1], ann[0])
 
     # Loop over genes and plot
-    gene_with_neighbours = generate_gene_with_neighbours(ann)
-    gene_with_neighbours = list(gene_with_neighbours)
-    print(gene_with_neighbours[0])
-    # contextualize_coordinates(gene_with_neighbours, chr_len, args.ext)
+    gene_with_neighbours = list(generate_gene_with_neighbours(ann))
     for i, (prev, gene, next) in enumerate(gene_with_neighbours, start=1):
-        logging.info(f"Processing {gene}")
-        prev, gene, next = contextualize_features([prev, gene, next], chr_len)
-        print(prev, gene, next)
-        region_limits = get_region_limits(gene, args.ext)
-        # print(region_limits)
-        region_limits = contextualize_coordinates(*region_limits, chr_len)
-        print(region_limits)
-        region_size = get_region_size(region_limits)
-        # print(region_size)
-        fragment_size = get_fragment_size(gene)
-        # print(fragment_size)
-        plot_data = get_plot_data(coverage_data, region_limits)
+        logging.info(f"Processing {gene}") 
+        sense_cov = get_coverage_context(p1, gene, chr_len)
+        anti_cov = get_coverage_context(m1, gene, chr_len)
 
-        # Plot comparison of coverage between the samples and strands
-        fig, ax1 = plot_base_line_graph(plot_data, region_limits)
+        # plot
+        fig, ax1 = plt.subplots()
+
+        plot_coverage(ax1, sense_cov, gene, line_style='b-')
+        plot_coverage(ax1, anti_cov, gene, line_style='r-')
         mark_gene_boundaries(gene)
+        mark_chr_len(gene, chr_len)
         highlight_neighbouring_genes(ax1, (prev, gene, next), chr_len)
-        add_gene_arrows(ax1, gene, region_size, fragment_size)
-        add_plot_title(gene, fragment_size)
-        update_x_axis(ax1, chr_len, region_size)
+        add_gene_arrows(ax1, gene)
+        update_x_axis(ax1, chr_len)
+        add_plot_title(gene)
 
         write_plot_to_file(save_path, gene, i, num_ann=len(ann))
         plt.clf()
         plt.close('all')
-        break
 
 if __name__ == "__main__":
     main()
